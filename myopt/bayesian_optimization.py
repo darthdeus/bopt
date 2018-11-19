f2673397bfc82da66a7a44406db784706d1ccc87
Jakub Arnold <darthdeus@gmail.com>
2018-11-19 14:10:56 +0100

    Minor cleanup, everything still works

--- myopt/bayesian_optimization.py
+++ myopt/bayesian_optimization.py
@@ -60,22 +60,77 @@ class OptimizationResult:
         return f"OptimizationResult(best_x={self.best_x}, best_y={self.best_y})"
 
 
+def default_from_bounds(bounds: List[Bound]) -> np.ndarray:
+    x_0 = np.zeros(len(bounds))
+
+    for i, bound in enumerate(bounds):
+        if bound.type == "int":
+            x_0[i] = np.random.randint(bound.low, bound.high)
+        elif bound.type == "float":
+            x_0[i] = np.random.uniform(bound.low, bound.high)
+        else:
+            raise RuntimeError(f"Invalid type of bound, got {type(bound)}")
+
+    return x_0
+
+
+def bo_minimize_parallel(f: Callable[[np.array], float], bounds: List[Bound],
+                         kernel: Kernel = SquaredExp(), acquisition_function=expected_improvement,
+                         x_0: np.ndarray = None, gp_noise: float = 0,
+                         n_iter: int = 8, callback: Callable = None,
+                         optimize_kernel=True, n_parallel=3) -> OptimizationResult:
+    if x_0 is None:
+        x_0 = default_from_bounds(bounds)
+    else:
+        for i, bound in enumerate(bounds):
+            assert bound.low <= x_0[i] <= bound.high, f"x_0 not in bounds, {bound} at {i}"
+
+    kernel = kernel.with_bounds(bounds)
+
+    y_0 = f(x_0)
+
+    assert type(y_0) == float, "f(x) must return a float"
+
+    X_sample = np.array([x_0])
+    y_sample = np.array([y_0])
+
+    for iter in range(n_iter - 1):
+        gp = GaussianProcess(kernel=kernel, noise=gp_noise).fit(X_sample, y_sample)
+        if optimize_kernel:
+            gp = gp.optimize_kernel()
+
+        multiple_x_next: List[np.ndarray] = \
+            propose_multiple_locations(acquisition_function, gp, y_sample.max(), bounds)
+
+        x_next = propose_location(acquisition_function, gp, y_sample.max(), bounds)
+
+        y_next = f(x_next)
+
+        if callback is not None:
+            callback(iter, acquisition_function, gp, X_sample, y_sample, x_next, y_next)
+
+        X_sample = np.vstack((X_sample, x_next))
+        y_sample = np.hstack((y_sample, y_next))
+
+    max_y_ind = y_sample.argmax()
+    print("max_x", X_sample[max_y_ind], "max max", y_sample.max())
+
+    return OptimizationResult(X_sample,
+                              y_sample,
+                              best_x=X_sample[y_sample.argmax()],
+                              best_y=y_sample.max(),
+                              bounds=bounds,
+                              kernel=kernel.copy())
+
+
 # TODO: should be bo_maximize :)
 def bo_minimize(f: Callable[[np.array], float], bounds: List[Bound],
                 kernel: Kernel = SquaredExp(), acquisition_function=expected_improvement,
                 x_0: np.ndarray = None, gp_noise: float = 0,
                 n_iter: int = 8, callback: Callable = None,
-                optimize_kernel=True):
+                optimize_kernel=True) -> OptimizationResult:
     if x_0 is None:
-        x_0 = np.zeros(len(bounds))
-
-        for i, bound in enumerate(bounds):
-            if bound.type == "int":
-                x_0[i] = np.random.randint(bound.low, bound.high)
-            elif bound.type == "float":
-                x_0[i] = np.random.uniform(bound.low, bound.high)
-            else:
-                raise RuntimeError(f"Invalid type of bound, got {type(bound)}")
+        x_0 = default_from_bounds(bounds)
     else:
         for i, bound in enumerate(bounds):
             assert bound.low <= x_0[i] <= bound.high, f"x_0 not in bounds, {bound} at {i}"
@@ -115,22 +170,23 @@ def bo_minimize(f: Callable[[np.array], float], bounds: List[Bound],
                               kernel=kernel.copy())
 
 
-def propose_location(acquisition: Callable, gp: GaussianProcess, y_max: float, bounds: List[Bound], n_restarts: int=25):
-    min_val = 1
-    min_x = None
+AcquisitionFunction = Callable[[GaussianProcess, np.ndarray, float], np.ndarray]
 
+
+def propose_location(acquisition: AcquisitionFunction, gp: GaussianProcess, y_max: float,
+                     bounds: List[Bound], n_restarts: int = 25):
     def min_obj(X):
-        # Minimization objective is the negative acquisition function
-        val = -acquisition(gp, X.reshape(1, -1), y_max)
-        return val
+        return -acquisition(gp, X.reshape(1, -1), y_max)
+
+    scipy_bounds = [(bound.low, bound.high) for bound in bounds]
 
     starting_points = []
     for _ in range(n_restarts):
         starting_points.append(np.array([bound.sample() for bound in bounds]))
 
-    scipy_bounds = [(bound.low, bound.high) for bound in bounds]
+    min_val = 1
+    min_x = None
 
-    # Find the best optimum by starting from n_restart different random points.
     for x0 in starting_points:
         res = minimize(min_obj, x0=x0, bounds=scipy_bounds, method='L-BFGS-B')
         if res.fun < min_val:
@@ -205,7 +261,7 @@ def bo_plot_exploration(f: Callable[[np.ndarray], float],
 #     # plot_convergence(X_sample, y_sample)
 
 
-def plot_2d_optim_result(result: OptimizationResult, resolution: float = 0.3, figsize=(8,7)):
+def plot_2d_optim_result(result: OptimizationResult, resolution: float = 0.3, figsize=(8, 7)):
     assert len(result.bounds) == 2
 
     b1 = result.bounds[0]
@@ -214,17 +270,16 @@ def plot_2d_optim_result(result: OptimizationResult, resolution: float = 0.3, fi
     x1 = np.arange(b1.low, b1.high, resolution)
     x2 = np.arange(b2.low, b2.high, resolution)
 
-
     gx, gy = np.meshgrid(x1, x2)
 
     X_2d = np.c_[gx.ravel(), gy.ravel()]
 
-    mu, _ = GaussianProcess(kernel=result.kernel.with_bounds(result.bounds))\
-                            .fit(result.X_sample, result.y_sample).posterior(X_2d).mu_std()
+    mu, _ = GaussianProcess(kernel=result.kernel.with_bounds(result.bounds)) \
+        .fit(result.X_sample, result.y_sample).posterior(X_2d).mu_std()
 
     plt.figure(figsize=figsize)
     plt.title("GP posterior")
     plt.imshow(mu.reshape(gx.shape[0], gx.shape[1]),
                extent=[b1.low, b1.high, b2.low, b2.high])
-    plt.scatter(result.X_sample[:,0], result.X_sample[:,1], c="k")
+    plt.scatter(result.X_sample[:, 0], result.X_sample[:, 1], c="k")
     plt.scatter([result.best_x[0]], [result.best_x[1]], c="r")
