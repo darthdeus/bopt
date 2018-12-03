c1e79a102ed617a45ed08f3bf7ea20fbee8035b5
Jakub Arnold <darthdeus@gmail.com>
2018-12-03 01:10:41 +0100

    Probability of improvement working

--- myopt/acquisition_functions.py
+++ myopt/acquisition_functions.py
@@ -22,3 +22,19 @@ def expected_improvement(gp: GaussianProcess, X: np.ndarray, f_s: float, xi: flo
     ei = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
 
     return ei
+
+
+def probability_of_improvement(gp: GaussianProcess, X: np.ndarray, f_s: float, xi: float=0.01) -> np.ndarray:
+    assert X is not None
+
+    mu, sigma = gp.posterior(X).mu_std()
+
+    mu = mu.reshape(-1, 1)
+    sigma = sigma.reshape(-1, 1) + 1e-6
+
+    improvement = mu - f_s - xi
+    Z = improvement / sigma
+
+    return norm.cdf(Z)
+
+
