9f21aaa5276186e3de6c8123c8670ca0f9acfce7
Jakub Arnold <darthdeus@gmail.com>
2018-12-09 02:14:48 +0100

    Move even more stuff out of jupyter

--- misc/gp_images.py
+++ misc/gp_images.py
@@ -1,7 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from myopt.kernel import Matern, SquaredExp, RationalQuadratic
 from myopt.gaussian_process import GaussianProcess
+
+# Basic images
+
+X_train = np.array([0, 0.3, 2, 4])
+y_train = np.sin(X_train)
+
+X = np.arange(min(X_train) - 0.5, max(X_train) + 0.5, step=0.1)
+
+GaussianProcess().fit(np.array([]),
+        np.array([])).optimize_kernel().posterior(X).plot_posterior()
+
+GaussianProcess(kernel=Matern(sigma=10, ro=5))\
+        .posterior(X, X_train, y_train).plot_posterior(num_samples=0)\
+        .optimize_kernel().posterior(X).plot_posterior()
+
+for k in [SquaredExp(), RationalQuadratic()]: #, Linear()]:
+    print(k)
+    GaussianProcess(kernel=k).plot_prior(X)
+
+for kernel in [RationalQuadratic(alpha=0.005)]: #, RationalQuadratic(), SquaredExp()]: #, Linear()]:
+    GaussianProcess(kernel=kernel, noise=0.02)\
+        .posterior(X, X_train, y_train).plot_posterior(num_samples=0)
+
+
+
+################################################
 
 X_train = np.array([0, 0.3, 2, 4])
 y_train = np.sin(X_train)
