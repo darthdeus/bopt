ae31d010d26406e7c199dd5b13926287b521cdbe
Jakub Arnold <darthdeus@gmail.com>
2019-01-16 17:51:41 +0100

    Remove broken tests

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import pytest
 
-from myopt.kernels import Linear
+from bopt.kernels import Linear
 
 
 class TestKernel(unittest.TestCase):
@@ -16,22 +16,3 @@ class TestKernel(unittest.TestCase):
         d = aa + bb
 
         self.assertTrue(np.allclose(c, d))
-
-    # def test_linear(self):
-        # linear = Linear()
-        # y = linear(np.array([[2]]), np.array([[3]]))[0].item()
-        # print(y)
-        # self.assertAlmostEqual(6, y)
-        #
-        # a = np.array([2])
-        # b = np.array([3])
-        # self.assertAlmostEqual(6, linear(a, b)[0].item())
-        #
-        # simple_expansion = linear(np.array([[2, 5]]), np.array([[3, 7]]))
-        # self.assertAlmostEqual(2*3 + 5*7, simple_expansion[0].item())
-        #
-        # multiple = linear(np.array([[2, 5], [3, 1]]), np.array([[3, 7], [2, 1]]))
-        # self.assertAlmostEqual(2*3 + 5*7, multiple[0, 0].item())
-        # self.assertAlmostEqual(2*2 + 5*1, multiple[0, 1].item())
-        # self.assertAlmostEqual(3*3 + 1*7, multiple[1, 0].item())
-        # self.assertAlmostEqual(3*2 + 1*1, multiple[1, 1].item())
