fec1273e8a84197058183f995fc091140e761599
Jakub Arnold <darthdeus@gmail.com>
2019-03-07 03:53:24 +0100

    Kernel serialization WIP

--- bopt/models/parameters.py
+++ bopt/models/parameters.py
@@ -1,21 +1,24 @@
 class ModelParameters:
     model_name: str
     params: dict
+    kernel: str
 
-    def __init__(self, model_name: str, params: dict) -> None:
+    def __init__(self, model_name: str, params: dict, kernel: str) -> None:
         self.model_name = model_name
         self.params = params
+        self.kernel = kernel
 
     def to_dict(self) -> dict:
         return {
             "model_name": self.model_name,
             "params": self.params,
+            "kernel": self.kernel
         }
 
     @staticmethod
     def from_dict(data: dict) -> "ModelParameters":
-        return ModelParameters(data["model_name"], data["params"])
+        return ModelParameters(data["model_name"], data["params"], data["kernel"])
 
     @staticmethod
     def manual_run() -> "ModelParameters":
-        return ModelParameters("manual", {})
+        return ModelParameters("manual", {}, None)
