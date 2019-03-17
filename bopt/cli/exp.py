0ace734e9f0d503cd44b3cedd2b1121a9027ccbf
Jakub Arnold <darthdeus@gmail.com>
2019-03-17 20:34:49 +0100

    Fix 'bopt exp'

--- bopt/cli/exp.py
+++ bopt/cli/exp.py
@@ -28,20 +28,24 @@ def run(args) -> None:
                 best_sample = sample
 
     print("objective: {}".format(best_res))
-    print(best_sample.job.run_parameters)
-    print()
 
-    print("Evaluations:")
-    for sample in experiment.samples:
-        job = sample.job
+    assert best_sample is not None
 
-        proc_stats = ""
-        if psutil.pid_exists(job.job_id):
-            process = psutil.Process(job.job_id)
-            mem = process.memory_info()
-            proc_stats += f"Process:{process.status()}"
-            proc_stats += f", cpu={process.cpu_percent()}"
-            # TODO fix this on osx, shared={mem.shared}"
-            proc_stats += f", rss={mem.rss}, vms={mem.vms}"
+    if best_sample.job is not None:
+        print(best_sample.job.run_parameters)
+        print()
 
-        print(f"{sample}\t{proc_stats}")
+        print("Evaluations:")
+        for sample in experiment.samples:
+            job = sample.job
+
+            proc_stats = ""
+            if psutil.pid_exists(job.job_id):
+                process = psutil.Process(job.job_id)
+                mem = process.memory_info()
+                proc_stats += f"Process:{process.status()}"
+                proc_stats += f", cpu={process.cpu_percent()}"
+                # TODO fix this on osx, shared={mem.shared}"
+                proc_stats += f", rss={mem.rss}, vms={mem.vms}"
+
+            print(f"{sample}\t{proc_stats}")
