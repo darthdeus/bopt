f0918e9c260164b31b133a227aab6138726f5a2b
Jakub Arnold <darthdeus@gmail.com>
2019-03-22 23:44:50 +0100

    Upgrade pyyaml

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     install_requires=[
         "numpy>=1.15.4",
         "scipy>=1.1.0",
-        "pyyaml>=4.2b1",
+        "pyyaml>=5.1",
         "tqdm~=4.28.1",
         "flask~=1.0.2",
         "psutil~=5.4.8",
